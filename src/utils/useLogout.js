7ab29a06158171340666f233d35e6d0e3dd93c22
Krisztina Tesenyi <61443013+KrisztaT@users.noreply.github.com>
2023-07-12 14:32:34 +1000

    Logout setup

---
+++ src/utils/useLogout.js
@@ -0,0 +1,18 @@
+import { useAuthContext } from "./useAuthContext";
+
+export const useLogout = () => {
+
+    const {dispatch} = useAuthContext()
+
+    const logout = () => {
+      
+        // delete user from storage
+        localStorage.removeItem('user')
+
+        // update global state
+        dispatch({type: 'LOGOUT'})
+
+    }
+
+return {logout}
+}
