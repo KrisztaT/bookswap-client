77df18fe2048426b9e7e5bdb051011c19ffe517c
Krisztina Tesenyi <61443013+KrisztaT@users.noreply.github.com>
2023-07-12 10:51:37 +1000

    Login and join forms in modals set up

--- src/components/JoinModal.js
+++ src/components/JoinModal.js
@@ -2,77 +2,90 @@ import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import { motion } from "framer-motion";
-import "../styles/Button.css"
-import "../styles/Logo.css"
-import "../styles/PageHeader.css"
-import "../styles/FormInput.css"
-import "../styles/Modal.css"
+import { useState } from "react";
+import "../styles/Button.css";
+import "../styles/Logo.css";
+import "../styles/PageHeader.css";
+import "../styles/FormInput.css";
+import "../styles/Modal.css";
 
 const JoinModal = ({ show, handleClose }) => {
+  const [username, setUsername] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, firstName, email, password);
+  };
+
   return (
     <>
-      <Modal show={show} onHide={handleClose} backdrop="static" >
-        <Modal.Header closeButton className="bg-light d-flex flex-column align-items-center justify-content-center">
-            <img src="./bookswap_logo.svg" alt="logo" className="logo" />
+      <Modal show={show} onHide={handleClose} backdrop="static">
+        <Modal.Header
+          closeButton
+          className="bg-light d-flex flex-column align-items-center justify-content-center"
+        >
+          <img src="./bookswap_logo.svg" alt="logo" className="logo" />
         </Modal.Header>
         <Modal.Body className="d-flex flex-column align-items-center justify-content-center modal-body">
-            <p className="page-header">Join BookSwap</p>
-          <Form className="">
-            <Form.Group
-              className=""
-              controlId="exampleForm.ControlInput1"
-            >
+          <p className="page-header">Join BookSwap</p>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="" controlId="exampleForm.ControlInput1">
               <Form.Control
                 type="text"
                 placeholder="Username"
                 autoFocus
                 className="form-input"
+                onChange={(e) => setUsername(e.target.value)}
+                input={username}
               />
             </Form.Group>
-            <Form.Group
-              className=""
-              controlId="exampleForm.ControlInput1"
-            >
+            <Form.Group className="" controlId="exampleForm.ControlInput1">
               <Form.Control
                 type="text"
                 placeholder="First Name"
                 className="form-input"
+                onChange={(e) => setFirstName(e.target.value)}
+                input={firstName}
               />
             </Form.Group>
-            <Form.Group
-              className=""
-              controlId="exampleForm.ControlInput1"
-            >
+            <Form.Group className="" controlId="exampleForm.ControlInput1">
               <Form.Control
-                type="text"
+                type="email"
                 placeholder="Email"
                 className="form-input"
+                onChange={(e) => setEmail(e.target.value)}
+                input={email}
               />
             </Form.Group>
-            <Form.Group
-              className=""
-              controlId="exampleForm.ControlInput1"
-            >
+            <Form.Group className="" controlId="exampleForm.ControlInput1">
               <Form.Control
                 type="text"
                 placeholder="Password"
                 className="form-input"
+                onChange={(e) => setPassword(e.target.value)}
+                input={password}
               />
             </Form.Group>
-            
+
+            <motion.div
+              className="d-flex flex-column align-items-center justify-content-center"
+              initial={{ rotateZ: 0 }}
+              animate={{
+                rotateZ: [0, -15, 10, -10, 6, -4, 0],
+                transition: {
+                  duration: 1,
+                  repeat: 3,
+                },
+              }}
+            >
+              <Button type="submit" className="btn-custom m-5">
+                Join
+              </Button>
+            </motion.div>
           </Form>
-          <motion.div
-            initial={{ rotateZ: 0 }}
-            animate={{
-              rotateZ: [0, -15, 10, -10, 6, -4, 0],
-              transition: {
-                duration: 1,
-                repeat: 3,
-              },
-            }}
-          >
-            <Button className="btn-custom m-4">Join</Button>
-          </motion.div>
         </Modal.Body>
       </Modal>
     </>
--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -2,56 +2,70 @@ import Form from "react-bootstrap/Form";
 import Modal from "react-bootstrap/Modal";
 import Button from "react-bootstrap/Button";
 import { motion } from "framer-motion";
-import "../styles/Button.css"
-import "../styles/Logo.css"
-import "../styles/PageHeader.css"
-import "../styles/FormInput.css"
-import "../styles/Modal.css"
+import { useState } from "react";
+import "../styles/Button.css";
+import "../styles/Logo.css";
+import "../styles/PageHeader.css";
+import "../styles/FormInput.css";
+import "../styles/Modal.css";
 
 const LoginModal = ({ show, handleClose }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+  };
+
   return (
     <>
-      <Modal show={show} onHide={handleClose} backdrop="static" >
-        <Modal.Header closeButton className="bg-light d-flex flex-column align-items-center justify-content-center">
-            <img src="./bookswap_logo.svg" alt="logo" className="logo" />
+      <Modal show={show} onHide={handleClose} backdrop="static">
+        <Modal.Header
+          closeButton
+          className="bg-light d-flex flex-column align-items-center justify-content-center"
+        >
+          <img src="./bookswap_logo.svg" alt="logo" className="logo" />
         </Modal.Header>
         <Modal.Body className="d-flex flex-column align-items-center justify-content-center modal-body">
-            <p className="page-header">Log in to BookSwap</p>
-          <Form className="">
-            <Form.Group
-              className="mt-4"
-              controlId="exampleForm.ControlInput1"
-            >
+          <p className="page-header">Log in to BookSwap</p>
+          <Form onSubmit={handleSubmit}>
+            <Form.Group className="mt-4" controlId="exampleForm.ControlInput1">
               <Form.Control
                 type="text"
                 placeholder="Username"
                 autoFocus
                 className="form-input"
+                onChange={(e) => setUsername(e.target.value)}
+                input={username}
               />
             </Form.Group>
-            <Form.Group
-              className="mt-4"
-              controlId="exampleForm.ControlInput1"
-            >
+            <Form.Group className="mt-4" controlId="exampleForm.ControlInput1">
               <Form.Control
                 type="text"
                 placeholder="Password"
                 className="form-input"
+                onChange={(e) => setPassword(e.target.value)}
+                input={password}
               />
             </Form.Group>
+
+            <motion.div
+              className="d-flex flex-column align-items-center justify-content-center"
+              initial={{ rotateZ: 0 }}
+              animate={{
+                rotateZ: [0, -15, 10, -10, 6, -4, 0],
+                transition: {
+                  duration: 1,
+                  repeat: 3,
+                },
+              }}
+            >
+              <Button type="submit" className="btn-custom m-5">
+                Login
+              </Button>
+            </motion.div>
           </Form>
-          <motion.div
-            initial={{ rotateZ: 0 }}
-            animate={{
-              rotateZ: [0, -15, 10, -10, 6, -4, 0],
-              transition: {
-                duration: 1,
-                repeat: 3,
-              },
-            }}
-          >
-            <Button className="btn-custom m-5">Login</Button>
-          </motion.div>
         </Modal.Body>
       </Modal>
     </>
