3af02fc21354c5f6e33e7d9f4ad23c176d3aab97
Krisztina Tesenyi <61443013+KrisztaT@users.noreply.github.com>
2023-07-19 18:29:59 +1000

    Added useSearchBook hook and comments
    Implemented useSearchBook hook for fetching the API endpoint to search book by title and author and return if it exists or not. The hook is used in the useAddBook hook, so now a book only added if it does not exits in the database. Additionally, I commented both the useAddBook and useSearchBook hooks.

--- src/utils/useAddBook.js
+++ src/utils/useAddBook.js
@@ -1,17 +1,24 @@
 import { useState } from "react";
 import { useBookAndListingContext } from "./useBookAndListingContext";
 import { useAuthContext } from "../utils/useAuthContext";
+import { useSearchBook } from "./useSearchBook";
 
+// retrieve the hostname of the current URL.
 const hostname = window.location.hostname;
+// if the hostname is localhost the dev backend is used if not the prod backend is used
 const api =
   hostname === "localhost"
     ? process.env.REACT_APP_DEV_BACKEND_URL
     : process.env.REACT_APP_PROD_BACKEND_URL;
 
+// add book hook is for create book in case it doesn't exist in the database
 export const useAddBook = () => {
+    // use both auth and book and listing context
   const { user } = useAuthContext();
   const { dispatch } = useBookAndListingContext();
+  const { searchBook } = useSearchBook();
 
+  // create error and loading states, which will be used on the Add Book form
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(null);
 
@@ -19,32 +26,45 @@ export const useAddBook = () => {
     setLoading(true);
     setError(null);
 
+    // only logged in users can access this functionality
     if (!user) {
       setError("Please login.");
       return;
     }
 
-    // fetch book endpoint to add book
-    const response = await fetch(`${api}/api/book`, {
-      method: "POST",
-      body: JSON.stringify({ imgUrl, title, author, page, releaseYear }),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${user.token}`,
-      },
-    });
-    const result = await response.json();
-    console.log(result);
+    // call the searchBook function and passing in the title,author, setError, and setLoading as
+    // arguments. It is then assigning the result of the function call to the variable res.
+    const res = await searchBook(title, author, setError, setLoading);
+    if (!res.exists) {
+      // fetch book endpoint to add book
+      const response = await fetch(`${api}/api/book`, {
+        method: "POST",
+        body: JSON.stringify({ imgUrl, title, author, page, releaseYear }),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
+      //parse the response from the API request into JSON format.
+      const result = await response.json();
+      console.log(result);
 
-    if (!response.ok) {
-      setLoading(false);
-      setError(result.error);
-    }
-    if (response.ok) {
-      // update context
-      dispatch({ type: "ADD_BOOK", payload: result });
+      // check if the response from the API request is not successful
+      // set loading to false and set error to error message
+      if (!response.ok) {
+        setLoading(false);
+        setError(result.error);
+      }
+      // if response is ok
+      if (response.ok) {
+        // update context
+        dispatch({ type: "ADD_BOOK", payload: result });
 
-      // data loading is finished
+        // data loading is finished
+        setLoading(false);
+      }
+    } else {
+      // if the book already exists, then the loading is finished, hence it set to false
       setLoading(false);
     }
   };
