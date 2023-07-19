63f4e12f760a37faf91e58b0725ba15bdbc87458
Krisztina Tesenyi <61443013+KrisztaT@users.noreply.github.com>
2023-07-19 14:24:06 +1000

    Added AddBook component
    The "AddBook" form component is for enabling lenders to  add books in their listings for lending purposes once the logic is included.

--- src/pages/LendingPage.js
+++ src/pages/LendingPage.js
@@ -1,9 +1,10 @@
+import AddBook from "../components/AddBook";
 
 function LendingPage() {
     
     return (
       <div className="my-5 py-5">
-          <p>This is the lending page</p>
+          <AddBook />
       </div>
     );
   }
