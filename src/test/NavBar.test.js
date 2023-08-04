b7564875d89a3b5ecbdf7086be48f5b7e7fe2d1f
Krisztina Tesenyi <61443013+KrisztaT@users.noreply.github.com>
2023-08-04 14:25:49 +1000

    Added LendingPage test and updated NavBar test

--- src/test/NavBar.test.js
+++ src/test/NavBar.test.js
@@ -11,8 +11,8 @@ jest.mock("../hooks/useAuthContext", () => ({
 
 // unit test for NavBar
 describe("Unit test for NavBar render with Login and Join Button", () => {
-  // unit test for NavBar render with Login and Join Button
-  test("Navigation bar contains Login and Join buttons", () => {
+  // unit test for NavBar render with Login Button
+  test("Navigation bar contains Login button when not authenticated", () => {
     // mock the return value of the useAuthContext hook
     useAuthContext.mockReturnValue({
       isAuthenticated: false,
@@ -31,13 +31,32 @@ describe("Unit test for NavBar render with Login and Join Button", () => {
     // get the login button element from the screen using the text Login
     const loginButton = screen.getByText("Login");
 
-    // get the join button element from the screen using the text Join
-    const joinButton = screen.getByText("Join");
-
     // it is expected that the loginButton element is present in the document
     expect(loginButton).toBeInTheDocument();
+  });
+
+  // unit test for NavBar render with Join Button
+  test("Navigation bar contains Join button when not authenticated", () => {
+    // mock the return value of the useAuthContext hook
+    useAuthContext.mockReturnValue({
+      isAuthenticated: false,
+      login: jest.fn(),
+      join: jest.fn(),
+    });
+
+    // render NavBar with default renderer
+    render(
+      // mock Router used for wrapping the NavBar
+      <MemoryRouter>
+        <NavBar />
+      </MemoryRouter>
+    );   
+
+    // get the join button element from the screen using the text Join
+    const joinButton = screen.getByText("Join");
 
     // it is expected that the joinButton element is present in the document
     expect(joinButton).toBeInTheDocument();
   });
+
 });
